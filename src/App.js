2df60abb3f999de0a9424a0bd55f0c5221fc33b8
“deep150499” <deepnanavati@icloud.com>
2024-02-14 19:43:37 -0500

    fetch and search feature

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
+import React , {useState , useEffect} from 'react';
 import './App.css';
+import ImageCard from './Components/ImageCard';
+import ImageSearch from './Components/ImageSearch';
 
 function App() {
+
+  const [images,setImages] = useState([]);
+  const [isLoading,setIsLoading] =  useState(true);
+  const [term,setTerm] = useState('');
+
+  const key =  '42380103-5dc76d0cb597f9b8794c0da63';
+
+  useEffect(() => {
+    fetch(`https://pixabay.com/api/?key=${key}&q=${term}&image_type=photo&pretty=true`)
+    .then(res => res.json())
+    .then(data => {
+      setImages(data.hits);
+      setIsLoading(false);
+    })
+    .catch(err => console.log(err));
+  },[term]);
+
   return (
-    
-    <div>Image</div>
+   <div className='container mx-auto'>
+    <ImageSearch searchText={(text) => setTerm(text)}/>
+    {!isLoading && images.length === 0  && <h1 className="text-5xl text-center mx-auto mt-32 ">No images found</h1>}
+    { isLoading ? <h1 className="text-6xl text-center mx-auto mt-32 ">Loading...</h1> : <div className='grid grid-cols-3 gap-4'>
+      {images.map(image => (
+        <ImageCard key={image.id} image={image}/>
+      ))}
+    </div>}
+   </div>
   );
 }
 
